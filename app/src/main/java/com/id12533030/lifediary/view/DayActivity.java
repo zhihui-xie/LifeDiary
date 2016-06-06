8533e8a5da0629a221a400ddcc421a55dc5fbcda
LemonT <463915634@qq.com>
2016-06-06 16:02:38 +1000

    Euclid failed

--- app/src/main/java/com/id12533030/lifediary/view/DayActivity.java
+++ app/src/main/java/com/id12533030/lifediary/view/DayActivity.java
@@ -5,30 +5,72 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
 
 import com.id12533030.lifediary.model.Day;
 import com.id12533030.lifediary.util.MainMenu;
 import com.id12533030.lifediary.R;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.yalantis.euclid.library.EuclidActivity;
+import com.yalantis.euclid.library.EuclidListAdapter;
 
 /**
  * Created by LENOVO on 2016/5/31.
  */
-public class DayActivity extends AppCompatActivity {
+public class DayActivity extends EuclidActivity {
     private MainMenu mMainMenu;
     private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.day_main);
-        mFragmentManager = getSupportFragmentManager();
-        mMainMenu = new MainMenu(this, mFragmentManager, true, true);
-        mMainMenu.initSystemBar(this);
-
+//        setContentView(R.layout.day_main);
+//        mFragmentManager = getSupportFragmentManager();
+//        mMainMenu = new MainMenu(this, mFragmentManager, true, true);
+//        mMainMenu.initSystemBar(this);
+//
         Day day = new Day("", "Day", Calendar.getInstance().getTimeInMillis(), "Important day");
         day.save();
+        mButtonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DayActivity.this, "Oh hi!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected BaseAdapter getAdapter() {
+        Map<String, Object> profileMap;
+        List<Map<String, Object>> profilesList = new ArrayList<>();
+
+        int[] avatars = {
+                R.drawable.f1,
+                R.drawable.f2,
+                R.drawable.f3,
+                R.drawable.f4,
+                };
+
+//        String[] names = getResources().getStringArray(R.array.array_names);
+        String[] names = {"Day One", "Day Two", "Day Three", "Day Four"};
+        for (int i = 0; i < avatars.length; i++) {
+            profileMap = new HashMap<>();
+            profileMap.put(EuclidListAdapter.KEY_AVATAR, avatars[i]);
+            profileMap.put(EuclidListAdapter.KEY_NAME, names[i]);
+            profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_SHORT, getString(R.string.lorem_ipsum_short));
+            profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_FULL, getString(R.string.lorem_ipsum_long));
+            profilesList.add(profileMap);
+        }
+
+        return new EuclidListAdapter(this, R.layout.list_item, profilesList);
     }
 
     @Override
